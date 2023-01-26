f027a1d54b9752540433faf05aea47e51f8fbc28
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-26 20:04:23 +0100

    add: table_posts, posts_seeder

---
+++ database/seeders/PostsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class PostsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newPost = new Post();
+            $newPost->name = $faker->name();
+            $newPost->text = $faker->sentence(3);
+            $newPost->save();
+        }
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,15 @@
             <div class="col-md-8">
                 <div class="card">
 
-                    {{ __('You are logged in!') }}
+                    <p>
+                        {{ __('You are logged in!') }}
+                    </p>
+
+                    {{-- <div class="mt-5">
+                        <a href="{{ route('admin.index') }}">
+                            Vai all' index
+                        </a>
+                    </div> --}}
 
                 </div>
             </div>

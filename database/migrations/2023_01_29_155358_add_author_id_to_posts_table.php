867ea4b601f36292807dffc6928deae2367beb9e
FabrizioMisseri <misserifabrizio@gmail.com>
2023-01-29 17:05:22 +0100

    add: 1-to-many relations, column 'author_id' in posts-table

---
+++ database/migrations/2023_01_29_155358_add_author_id_to_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('author_id')->nullable()->after('id');
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign('posts_author_id_foreign');
+            $table->dropColumn('author_id');
+        });
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PostsSeeder::class,
             TagsSeeder::class,
+            AuthorsSeeder::class,
         ]);
     }
 }
